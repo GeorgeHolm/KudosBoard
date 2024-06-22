bc16921da36f6ebc085a47211a8edb9f5c99596e
GeorgeHolm <114369084+GeorgeHolm@users.noreply.github.com>
2024-06-21 19:07:29 -0700

    a

--- frontend/src/components/Boards.jsx
+++ frontend/src/components/Boards.jsx
@@ -116,7 +116,7 @@ const Boards = (props) => {
       method: "DELETE",
     })
       .then((response) => response.json())
-      .then(props.handleAll())//REPLACE CONSOLE LOG WITH PROPPED FETCH
+      .then(this.forceUpdate())//REPLACE CONSOLE LOG WITH PROPPED FETCH
       .catch((error) => console.error(error));
   };
 
